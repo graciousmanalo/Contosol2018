41440304f0c4030e72a59dbded747bb16fc04b33
graci <graci@DESKTOP-GC4R435>
2018-05-02 11:30:37 -0300

    Added Attribute

--- Contosol2018/Models/Instructor.cs
+++ Contosol2018/Models/Instructor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace ContosoUniversity2018.Models
     public class Instructor:Person //Instructor inherits from Person
     {
         //Instructor specific properties
+        [Display(Name = "Hire Date")] //always starts [] in annotation
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateTime HireDate { get; set; }
        
     }
